875203706a38bbd2808fdcb585652d49f169d014
Iqbal Syamil Ayasy <iqbal@pintu.co.id>
2021-06-13 13:07:41 +0700

    update readme & example app
    Signed-off-by: Iqbal Syamil Ayasy <iqbal@pintu.co.id>

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # POC: Nginx for loadbalancing & caching NodeJS Application
+
+## Demo
+1. ssh into nginx VM using vagrant cli
+`vagrant ssh nginx`
+2. run app using pm2
+`pm2 start index.js --name "{app-name}" -- {port} {app-name}`
+Example : `pm2 start index.js --name "app-01" -- 3022 app-1`
--- index.js
+++ index.js
@@ -1,31 +1,39 @@
 const express = require('express')
-const bp = require('body-parser')
 const moment = require('moment')
 
-const PORT = process.env.PORT || 3022
+const PORT = process.argv[2] || process.env.PORT || 3022
+const appName = process.argv[3] || 'horizon'
 const app = express()
-app.set('trust proxy', ['uniquelocal']) 
+app.set('trust proxy', ['uniquelocal'])
 
-app.get('/', (req,res) => {
+const commonHandler = (req,res) => {
     const timestamp = moment().format('MMMM Do YYYY, h:mm:ss a')
     const {getUrl, method, hostname, headers} = req
     const data = {
-        method,
-        hostname,
-        headers,
+        message: `hello world from ${appName}`,
+        requestPayload:{
+            getUrl,
+            method,
+            hostname,
+            headers
+        },
         timestamp
     }
     res.send(JSON.stringify(data))
-})
-app.get('/cached', (req,res) => {
+}
+app.get('/', commonHandler)
+app.get('/ipHash', commonHandler)
+app.get('/leastConn', commonHandler)
+
+app.get('/metadata', (req,res) => {
     const timestamp = moment().format('MMMM Do YYYY, h:mm:ss a')
-    const {getUrl, method, hostname, headers} = req
     const data = {
-        method,
-        hostname,
-        headers,
+        "description": "Just an example metadata.",
+        "external_url": "https://github.com/2pai/nft-erc721/blob/main/assets/2pai-japan.png",
+        "image": "https://github.com/2pai/nft-erc721/raw/main/assets/2pai-japan.png",
+        "name": "2Pai Japanese Edition",
         timestamp
-    }
+      }
     res.send(JSON.stringify(data))
 })
 app.listen(PORT,() => console.log(`This app is running on ${PORT}`))
